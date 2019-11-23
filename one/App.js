0041e4582e69f485ee51ddabb245f505ad24d071
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-11-24 00:04:05 +0530

    first expo

---
+++ one/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>So this is the first demo app I created using react native</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
