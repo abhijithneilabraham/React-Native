33e5b11484cb3a0c95619b4ca738ef632700bb96
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-11-24 11:35:04 +0530

    update buttons

--- one/App.js
+++ one/App.js
@@ -1,19 +1,70 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from 'react-native';
+import Constants from 'expo-constants';
+
+function Separator() {
+  return <View style={styles.separator} />;
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>So this is the first demo app I created using react native</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>
+          Enter Email ID
+        </Text>
+
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+
+        </Text>
+        <Button
+          title="Upload File"
+          color="#f194ff"
+          onPress={() => Alert.alert('Uploading your file')}
+        />
+      </View>
+      <Separator />
+      <View>
+
+        <Button
+          title="Download File"
+          disabled
+          onPress={() => Alert.alert('Cannot press this one')}
+        />
+      </View>
+      <Separator />
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
